2805b59c5fa68a58e783139449aacc518bac45dd
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-07-15 15:48:13 +0300

    Add folders structure

---
+++ lib/ui/pages/home_paga.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Build-link')),
+      body: const Center(
+        child: Text('Платформа для агентства недвижимости и агента'),
+      ),
+    );
+  }
+}
