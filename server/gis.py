496de2fca34d741fc1da98e68acba355c321c5f9
iamgirya <iamgirya@yandex.ru>
2022-07-16 03:27:06 +0300

    Запрос кард и инфы о доме

--- server/gis.py
+++ server/gis.py
@@ -1,38 +1,33 @@
 import psycopg2
 from psycopg2 import Error
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
-try:
-    # Подключение к существующей базе данных
-    connection = psycopg2.connect(user="user",
-                                  # пароль, который указали при установке PostgreSQL
-                                  password="password123",
-                                  host="127.0.0.1",
-                                  port="5432",
-                                  database="postgres")
-    connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-    # Курсор для выполнения операций с базой данных
-    cursor = connection.cursor()
-    # Распечатать сведения о PostgreSQL
-    print("Информация о сервере PostgreSQL")
-    print(connection.get_dsn_parameters(), "\n")
-    # Выполнение SQL-запроса
-    sql_create_database = """
-CREATE TABLE public.ttttts
-(
-    id integer NOT NULL GENERATED ALWAYS AS IDENTITY,
-    PRIMARY KEY (id)
-);
 
-"""
-    cursor.execute(sql_create_database)
-    # Получить результат
-    record = cursor.fetchone()
-    print("Вы подключены к - ", record, "\n")
+def getHome(id):
+    return getRequest("""SELECT * FROM public.objects Where id = """ + str(id))
 
-except (Exception, Error) as error:
-    print("Ошибка при работе с PostgreSQL", error)
-finally:
-    if connection:
-        cursor.close()
-        connection.close()
-        print("Соединение с PostgreSQL закрыто")
+def getCards(id):
+    return getRequest("""SELECT firstname, lastname, patronymic, "number", note, id_agent
+	FROM public.cards WHERE id_agent = """ + str(id))
+
+def getRequest(sql_request):
+    try:
+        connection = psycopg2.connect(user="user",
+                                    password="password123",
+                                    host="127.0.0.1",
+                                    port="5432",
+                                    database="postgres")
+        connection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        # Курсор для выполнения операций с базой данных
+        cursor = connection.cursor()
+        # Выполнение SQL-запроса
+        cursor.execute(sql_request)
+        # Получить результат
+        records = cursor.fetchone()
+
+    except (Exception, Error) as error:
+        print("Ошибка при работе с PostgreSQL", error)
+    finally:
+        if connection:
+            cursor.close()
+            connection.close()
+    return records
