3a55c11edcd3bf6c4a22abea256f287b78f68472
StarProxima <34741787+StarProxima@users.noreply.github.com>
2022-07-16 11:44:41 +0300

    Fix font height

--- client/lib/data/styles/fonts.dart
+++ client/lib/data/styles/fonts.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 extension AppFontStyles on TextStyle {
   static TextStyle title = const TextStyle(
     fontFamily: "Montserrat",
+    height: 1.115,
   );
 
   static TextStyle label = const TextStyle(
